b8336188ea8ea098098f84d4bb6c947f9484ea22
muruganpandian <murugan.pandian@gmail.com>
2013-07-10 19:56:56 +0000

    updated web.js again 2

--- web.js
+++ web.js
@@ -3,7 +3,7 @@ var fs = require('fs');
 
 var app = express.createServer(express.logger());
 
-var buffer = new Buffer(fs.readFileSync(i'index.html'), 'utf-8');
+var buffer = new Buffer(fs.readFileSync('index.html'), 'utf-8');
 
 app.get('/', function(request, response) {
   response.send(buffer.toString());
